b302838356da9f79f6e04a68acf283fc23332ab1
Coderkoala <coderkoala@github.com>
2019-02-06 11:38:37 +0545

    Added comment

--- erpnext/education/doctype/program_enrollment/program_enrollment.py
+++ erpnext/education/doctype/program_enrollment/program_enrollment.py
@@ -123,19 +123,23 @@ def make_inv(customer, customer_name, due_date, courses, fees):
 	from frappe import utils
         import json
 	count = 0        
+	#courses and fees are lists that have the docnames from the front end
 	courses = json.loads(courses)
 	fees = json.loads(fees)
         udoc = frappe.new_doc("Sales Invoice")
         udoc.naming_series = "ACC-SINV-.YYYY.-"
         udoc.customer = customer
         udoc.customer_name = customer_name
-	#Just a convoluted mess, apologies, it just calls back to link the item with the fee
-	#category, and thereby make the sales invoice populated
+	#Fees
 	if(fees):
 		for d in fees:
+			#fetch fee_structure, it has the amount for the sales invoice, as well as..
 			fee_structure = frappe.get_doc("Fee Structure", d)
 			for e in fee_structure.components:
 				fee_amount = e.amount
+				#The fee category. This is have a new field for 
+				#"Items" doctype, which will make sure each
+				#course correlates to a proper item for identification
 				fetched_item = frappe.get_doc("Fee Category", e.fees_category)
 				if(fetched_item.item):
 					udoc.append('items', {
@@ -145,6 +149,10 @@ def make_inv(customer, customer_name, due_date, courses, fees):
 					'amount': fee_amount,
 					})
 				count = count + 1
+				#Only for checking if the sales invoice will turn out empty
+				#In case none of the fee category or the Course doctypes
+				#Have any items listed
+	#For course
 	if(courses):
 		for i in range(len(courses)):	
 			fdata = frappe.get_doc('Course', courses[i])		
@@ -155,6 +163,7 @@ def make_inv(customer, customer_name, due_date, courses, fees):
 				'qty': '1',
 				})
 	udoc.posting_date = frappe.utils.nowdate()
+	#Due date  = enrollment date	
 	udoc.due_date = due_date
 	#saves only if there is at least one potential tx
 	if( count>0 ):	
